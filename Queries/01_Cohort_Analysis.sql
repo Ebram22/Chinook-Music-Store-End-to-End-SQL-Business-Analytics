b84908e35cf36579273de1d0471f6e0b45d9f783
Ebram Wagdy <wagdyebram71@gmail.com>
2026-08-19 01:54:07 +0300

    Update 01_Cohort_Analysis.sql

--- Queries/01_Cohort_Analysis.sql
+++ Queries/01_Cohort_Analysis.sql
@@ -14,11 +14,11 @@ from Joined_year jy
 	inner join invoice i on jy.customerid=i.customerid
 )
 Select  Joined_Year,
-		count(distinct customerid) as Total_Customers,
-        SUM(CASE WHEN invoice_date = 2021 THEN Total_Sales ELSE 0 END) AS Sales_2021,
-		SUM(CASE WHEN invoice_date = 2022 THEN Total_Sales ELSE 0 END) AS Sales_2022,
-		SUM(CASE WHEN invoice_date = 2023 THEN Total_Sales ELSE 0 END) AS Sales_2023,
-		SUM(CASE WHEN invoice_date = 2024 THEN Total_Sales ELSE 0 END) AS Sales_2024,
-		SUM(CASE WHEN invoice_date = 2025 THEN Total_Sales ELSE 0 END) AS Sales_2025
+		Count(distinct customerid) as Total_Customers,
+        Sum(case when invoice_date = 2021 then Total_Sales else 0 end) as Sales_2021,
+		Sum(case when invoice_date = 2022 then Total_Sales else 0 end) as Sales_2022,
+		Sum(case when invoice_date = 2023 then Total_Sales else 0 end) as Sales_2023,
+		Sum(case when invoice_date = 2024 then Total_Sales else 0 end) as Sales_2024,
+		Sum(case when invoice_date = 2025 then Total_Sales else 0 end) as Sales_2025
 from Cohot_Analysis
 group by Joined_year
